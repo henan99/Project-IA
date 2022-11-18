37aa84408eed8465e089b4b9467a398781943b24
TobsTha <tobithallermb@gmail.com>
2022-11-18 17:40:58 +0100

    creation of folder 'Graphics' and adaption of file 'preprozessing'

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import keras
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
@@ -20,13 +21,16 @@ def data_as_vector():
     
     return featureVector, targets
 
-def scale_and_split(featureVector, targets):
+def scale_onehot_and_split(featureVector, targets):
     seed = 8+18+12 #fixed seed
-
-    scale = StandardScaler()
-    featureVector = scale.fit_transform(featureVector) # normalize data
-
     #fixed seed split 75-25
     X_train, X_test, y_train, y_test = train_test_split(featureVector, targets, test_size=0.25, random_state=seed)
 
-    return X_train, X_test, y_train, y_test
+    scale = StandardScaler()
+    X_train = scale.fit_transform(X_train) # normalize data
+    X_test = scale.transform(X_test)
+
+    y_train_onehot = keras.utils.to_categorical(y_train)
+    y_test_onehot = keras.utils.to_categorical(y_test)
+
+    return X_train, X_test, y_train_onehot, y_test_onehot
