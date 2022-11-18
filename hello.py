c12c5e2014ad168582b208e48f1fdeb3f66743f4
TobsTha <tobithallermb@gmail.com>
2022-11-18 17:38:22 +0100

    creation of folder "Graphics" and adaption of "preprocessing.py''

---
+++ hello.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from sklearn.decomposition import PCA
+
+
+#i added a comment lol
+
+
+def main():
+    print("hello world")
+    
+
+
+
+def new_function(x, z):
+    x = x+1
+    return x**2/z
+
+def new_func2(x):
+    y = np.zeros(x)
+    
+    return y
+y = new_func2(10)
+print(y)
+
+x = np.linspace(1,10)
+plt.figure(1)
+plt.plot(x,new_function(x))
+plt.show()
