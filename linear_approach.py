f32e4a366858c122dcf0702ac22dc2b39fa753d4
TobsTha <tobithallermb@gmail.com>
2022-11-25 17:30:00 +0100

    sans onehot

--- linear_approach.py
+++ linear_approach.py
@@ -2,10 +2,12 @@ import preprocessing as prep
 from sklearn.linear_model import LogisticRegressionCV
 import numpy as np
 
-X_train, X_test, y_train, y_test = prep.preprocess_without_onehot(seed=1)
+X_train, X_test, y_train, y_test = prep.preprocess(seed=3)
 
 C = np.logspace(-4, 4, 20)
 model = LogisticRegressionCV(Cs = C, penalty='l2')
 model.fit(X_train, y_train, sample_weight = None)
 score = model.score(X_test, y_test, sample_weight = None)
 print('score:', score)
+print('scores:', model.scores_)
+print('C:', model.C_)
